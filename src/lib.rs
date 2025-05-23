e59030887988878863264a6331c1192bfe3b445d
ReeseHatfield <hatfield.69@wright.edu>
2025-05-23 10:46:51 -0400

    proper run errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod check;
 pub mod github;
 pub mod init;
 pub mod run;
--- src/run.rs
+++ src/run.rs
@@ -1,16 +1,39 @@
 use std::{error::Error, process::Command};
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+pub enum OsedaRunError {
+    BuildError(String),
+    ServeError(String),
+}
+
+impl std::error::Error for OsedaRunError {}
+impl std::fmt::Display for OsedaRunError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::BuildError(msg) => write!(f, "Oseda Build Error: {}", msg),
+            Self::ServeError(msg) => write!(f, "Oseda Serve Error: {}", msg),
+        }
+    }
+}
+
+pub fn run() -> Result<(), OsedaRunError> {
+    // command run failure and command status are considered different, handled accordingly
     match Command::new("npx").arg("vite").arg("build").status() {
         Ok(status) => {
             if !status.success() {
                 println!("Error: `npx vite build` exited with a failure.");
                 println!("Please ensure that npx and vite are installed properly.");
+                return Err(OsedaRunError::BuildError(
+                    "could not 'npx vite build'".to_string(),
+                ));
             }
         }
         Err(e) => {
             println!("Error: failed to execute `npx vite build`: {e}");
             println!("Please ensure that `npx` and `vite` are installed and in your PATH.");
+            return Err(OsedaRunError::BuildError(
+                "could not 'npx vite build'".to_string(),
+            ));
         }
     }
 
@@ -21,11 +44,17 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                 println!(
                     "Please ensure that `serve` is installed properly (e.g., with `npm install -g serve`)."
                 );
+                return Err(OsedaRunError::ServeError(
+                    "could not 'serve dist'".to_string(),
+                ));
             }
         }
         Err(e) => {
             println!("Error: failed to execute `serve dist`: {e}");
             println!("Please ensure that `serve` is installed and in your PATH.");
+            return Err(OsedaRunError::ServeError(
+                "could not 'serve dist'".to_string(),
+            ));
         }
     }
 
