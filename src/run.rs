667d4b443b12bd83c7f7a328156cf733bc51299e
ReeseHatfield <hatfield.69@wright.edu>
2025-05-23 01:41:41 -0400

    run handle missing deps

--- src/run.rs
+++ src/run.rs
@@ -1,9 +1,19 @@
 use std::{error::Error, process::Command};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    Command::new("npx").arg("vite").arg("build").status()?;
+    let build_status = Command::new("npx").arg("vite").arg("build").status()?;
 
-    Command::new("serve").arg("dist").status()?;
+    if !build_status.success() {
+        println!("Error: could not run `npx vite build`");
+        println!("Please ensure that npx and vite are both installed properly")
+    }
 
+    let serve_status = Command::new("serve").arg("dist").status()?;
+
+    if !serve_status.success() {
+        println!("Error: could not run `serve dist`");
+        println!("Please ensure that npx and vite are both installed properly")
+        println!("with npm install -g serve")
+    }
     Ok(())
 }
