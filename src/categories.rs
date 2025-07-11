577ec7b88640e4fd49aeda8a34b718db7d7afca1
ReeseHatfield <hatfield.69@wright.edu>
2025-07-11 16:02:57 -0400

    add more categories

--- src/categories.rs
+++ src/categories.rs
@@ -2,12 +2,23 @@ use serde::{Deserialize, Serialize};
 use strum::IntoEnumIterator;
 use strum_macros::{Display, EnumIter};
 
-/// All possible course categoes
-/// TODO make this better
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Display, EnumIter)]
 pub enum Category {
+    Aerospace,
+    Business,
     ComputerScience,
+    Economics,
+    Education,
     Engineering,
+    Geography,
+    HealthMedicine,
+    History,
+    LanguageArts,
+    LiberalArts,
+    Mathematics,
+    Politics,
+    Psychology,
+    Science,
 }
 
 impl Category {
