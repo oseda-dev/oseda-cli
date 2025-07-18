23277677d7b209b940c540b1a15022f6c51d6fdb
ReeseHatfield <hatfield.69@wright.edu>
2025-07-17 22:44:42 -0400

    ubuntu test commands

---
+++ scripts/ubuntu-test-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is not meant directly executed
+# just the set of commands to install in a fresh ubuntu vm for testing
+
+sudo apt install curl
+
+sudo apt install npm
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+curl -sL https://raw.githubusercontent.com/oseda-dev/oseda-cli/refs/heads/main/scripts/curl-install.sh | $SHELL
