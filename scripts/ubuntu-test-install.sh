68c4e3521ea55eb2e1f1f26689cf0905e55f80f0
ReeseHatfield <hatfield.69@wright.edu>
2025-09-16 10:23:53 -0400

    add extra dep

--- scripts/ubuntu-test-install.sh
+++ scripts/ubuntu-test-install.sh
@@ -7,6 +7,8 @@ sudo apt install curl
 
 sudo apt install npm
 
+sudo apt install pkg-config
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 curl -sL https://raw.githubusercontent.com/oseda-dev/oseda-cli/refs/heads/main/scripts/curl-install.sh | $SHELL
