1f7d12fc2bfdd060b55f3e7bc7ce0757aae60e16
elmiller <miller.1771@wright.edu>
2025-10-22 21:03:40 -0400

    added yes pip to some commands, added comments of what to do

--- scripts/ubuntu-test-install.sh
+++ scripts/ubuntu-test-install.sh
@@ -3,12 +3,27 @@
 # This is not meant directly executed
 # just the set of commands to install in a fresh ubuntu vm for testing
 
+# if a sudo password is needed, how can this process be automated?
+# can it only be automated with the entire process to be automated?
 sudo apt install curl
 
-sudo apt install npm
+yes | sudo apt install npm
 
-sudo apt install pkg-config
+yes | sudo apt install pkg-config
 
+# TODO: use here-strings or here-documents to provide input to this command
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 curl -sL https://raw.githubusercontent.com/oseda-dev/oseda-cli/refs/heads/main/scripts/curl-install.sh | $SHELL
+
+# initialize a project
+# basically do everything except deploy a final project
+
+# open default presentation in browser
+
+oseda init
+
+oseda run
+
+# open local host in browser, look up command to do that 
+
