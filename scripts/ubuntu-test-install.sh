735b96c21f486ff52d0e814c81bde51c45bb01b4
22lavonne <22lavonne@gmail.com>
2025-11-07 15:11:05 -0500

    fixed rust install command to automatically install default settings

--- scripts/ubuntu-test-install.sh
+++ scripts/ubuntu-test-install.sh
@@ -11,16 +11,20 @@ yes | sudo apt install npm
 
 yes | sudo apt install pkg-config
 
-# TODO: use here-strings or here-documents to provide input to this command
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+#  restart the terminal after installing rust
+reset
 
 curl -sL https://raw.githubusercontent.com/oseda-dev/oseda-cli/refs/heads/main/scripts/curl-install.sh | $SHELL
 
+# TODO: 
 # initialize a project
 # basically do everything except deploy a final project
 
 # open default presentation in browser
-
+# not sure how to automate this part,
+# also need to write code to login to github account in order to initialize an oseda project
 oseda init
 
 oseda run
