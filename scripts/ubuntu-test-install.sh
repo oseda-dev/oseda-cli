4ebad848287a70da11f0d6f735c5983e9c0081b9
22lavonne <22lavonne@gmail.com>
2025-11-10 18:20:53 -0500

    added more descriptive comments, added todo comments

--- scripts/ubuntu-test-install.sh
+++ scripts/ubuntu-test-install.sh
@@ -3,31 +3,25 @@
 # This is not meant directly executed
 # just the set of commands to install in a fresh ubuntu vm for testing
 
-# if a sudo password is needed, how can this process be automated?
-# can it only be automated with the entire process to be automated?
+#TODO: make a MOCK_DATA.sh file to hold sudo password, as well as GitHub credentials
 sudo apt install curl
 
+# Pipe yes into these installation commands
 yes | sudo apt install npm
 
 yes | sudo apt install pkg-config
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-#  restart the terminal after installing rust
+# Restart the terminal after installing rust so it gets added to the path.
 reset
 
 curl -sL https://raw.githubusercontent.com/oseda-dev/oseda-cli/refs/heads/main/scripts/curl-install.sh | $SHELL
 
 # TODO: 
-# initialize a project
-# basically do everything except deploy a final project
+# Automate logging into a dummy GitHub account so a new oseda project can be initialized
+# Initialize and run a new oseda project in the user's browser
 
-# open default presentation in browser
-# not sure how to automate this part,
-# also need to write code to login to github account in order to initialize an oseda project
 oseda init
 
 oseda run
-
-# open local host in browser, look up command to do that 
-
