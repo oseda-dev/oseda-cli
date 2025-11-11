e7150eabc99ff1fabe9d9afbf41d9be5aa3c99d3
22lavonne <22lavonne@gmail.com>
2025-11-11 15:48:50 -0500

    loaded mock data into test script

--- scripts/e2e-testing/ubuntu-test-install.sh
+++ scripts/e2e-testing/ubuntu-test-install.sh
@@ -2,9 +2,10 @@
 
 # This is not meant directly executed
 # just the set of commands to install in a fresh ubuntu vm for testing
+source mock_data.sh
 
-#TODO: make a MOCK_DATA.sh file to hold sudo password, as well as GitHub credentials
-sudo apt install curl
+#TODO test to make sure this works
+$ADMIN_PASSWD |sudo -S apt install curl git
 
 # Pipe yes into these installation commands
 yes | sudo apt install npm
@@ -21,6 +22,18 @@ curl -sL https://raw.githubusercontent.com/oseda-dev/oseda-cli/refs/heads/main/s
 # TODO: 
 # Automate logging into a dummy GitHub account so a new oseda project can be initialized
 # Initialize and run a new oseda project in the user's browser
+
+# make sure .ssh directory exists
+mkdir -p "${HOME}/.ssh"
+# add public and private key
+echo $PUBLIC_KEY > "${HOME}/.ssh/id_ed25519_oseda_testing.pub"
+echo $PRIVATE_KEY > "${HOME}/.ssh/id_ed25519_oseda_testing"
+# change permissions for keys
+chmod 600 "${HOME}/.ssh/id_ed25519_oseda_testing.pub"
+chmod 600 "${HOME}/.ssh/id_ed25519_oseda_testing"
+
+# assuming git username is same as github username
+git config --global user.name $USERNAME
 
 oseda init
 
