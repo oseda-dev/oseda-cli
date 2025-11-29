23e69f0d509b8bc8d0ef3c9332292f9b9c36e81b
ReeseHatfield <reesehatfield02@gmail.com>
2025-11-29 11:43:14 -0500

    add code highlight to md template

--- src/static/md-templates/main.js
+++ src/static/md-templates/main.js
@@ -1,6 +1,10 @@
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import Notes from "reveal.js/plugin/notes/notes.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
+
+import "reveal.js/plugin/highlight/monokai.css";
+
 
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
@@ -21,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const deck = new Reveal({
     hash: true,
-    plugins: [Markdown, Notes],
+    plugins: [Markdown, Notes, Highlight],
   });
 
   deck.initialize();
