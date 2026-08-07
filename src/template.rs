c8b117a75ea445790689742f3fc221dea742b66e
ReeseHatfield <reesehatfield02@gmail.com>
2026-08-07 08:57:21 -0400

    html first in template iter

--- src/template.rs
+++ src/template.rs
@@ -4,6 +4,6 @@ use strum_macros::{Display, EnumIter, EnumString};
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Display, EnumIter, EnumString)]
 #[strum(ascii_case_insensitive)]
 pub enum Template {
-    Markdown,
     HTML,
+    Markdown,
 }
