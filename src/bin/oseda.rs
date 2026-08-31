790be08959e44d639c2fad1df94bab913155d30b
ReeseHatfield <reesehatfield02@gmail.com>
2026-08-30 21:00:00 -0400

    add update subcommand

--- src/bin/oseda.rs
+++ src/bin/oseda.rs
@@ -2,14 +2,13 @@ use std::{error::Error, process};
 
 use clap::Parser;
 use oseda_cli::{
-    cmd::{
+    Cli, Commands, cmd::{
         check,
         deploy::{self},
         export::{self},
         fork::{self},
-        init, run,
-    },
-    Cli, Commands,
+        init, run, update,
+    }
 };
 
 /// CLI entry point
@@ -34,6 +33,11 @@ fn main() {
         Commands::Fork => fork::fork(),
         Commands::Export(options) => export::export(options.clone())
             .map(|_| println!("Successfully export project to {0}", options.port)),
+        Commands::Update => update::update()
+            .map(|_| {
+                println!("Successfully updated oseda");
+                println!("You may need to restart the shell for updates to take effect")
+            }),
     };
 
     // little annoying, but makes the exit code match what users would expect
